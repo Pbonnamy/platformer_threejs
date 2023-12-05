c59b3c6142f890e6a9395228d5a7fb08268fffc3
Pbonnamy <bonnamy.77@gmail.com>
2023-12-05 12:35:34 +0100

    auto resize scene on window resize

--- platformer.js
+++ platformer.js
@@ -4,6 +4,8 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer();
 
+window.addEventListener('resize', onWindowResize, false);
+
 let cube;
 
 main();
@@ -12,6 +14,13 @@ function main()
 {
     init();
     createCube();
+}
+
+function onWindowResize() 
+{
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
 
