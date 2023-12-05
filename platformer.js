b37263c4fb7d6ce2822feacfa0a5321fc35c7275
Pbonnamy <bonnamy.77@gmail.com>
2023-12-05 12:31:00 +0100

    sample cube

--- platformer.js
+++ platformer.js
@@ -1,8 +1,42 @@
 import * as THREE from 'three';
 
-init();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer();
+
+let cube;
+
+main();
+
+function main() 
+{
+    init();
+    createCube();
+}
+
 
 function init() 
 {
-    const scene = new THREE.Scene();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+    camera.position.z = 5;
 }   
+
+function createCube() 
+{
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    animateCube();
+}
+
+function animateCube()
+{
+    requestAnimationFrame( animateCube );
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render( scene, camera );
+}
