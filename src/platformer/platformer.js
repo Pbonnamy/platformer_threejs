c44fb11395764a2d740a9fc343050a637b0e4548
Pierre <pbonnamy.pro@gmail.com>
2024-01-17 22:12:08 +0100

    adjustments

--- src/platformer/platformer.js
+++ src/platformer/platformer.js
@@ -114,8 +114,8 @@ function init() {
 
     //camera
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 20;
-    camera.position.y = 10;
+    camera.position.z = 19;
+    camera.position.y = 11;
 
     //camera controls
     controls = new OrbitControls(camera, renderer.domElement);
